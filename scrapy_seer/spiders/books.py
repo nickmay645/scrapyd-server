27e0c6b80dec3a92cdc131725e9be038afc8c383
nickmay645 <nickmay645@gmail.com>
2023-12-08 15:55:07 -0500

    init scrapy project

---
+++ scrapy_seer/spiders/books.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+        
+        for row in response.css("ol.row > li > article.product_pod"):
+            title = row.css("h3 > a::attr(title)").get()
+            price = row.css("div.product_price > p.price_color::text").get()
+            yield {"title": title, "price": price}
+
